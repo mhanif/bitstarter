b2ed2c62c921ea849bb1ff65fce804c459457099
Ubuntu <ubuntu@ip-172-31-12-89.us-west-1.compute.internal>
2013-07-21 00:34:40 +0000

    Fixes for minimal change

--- web.js
+++ web.js
@@ -1,16 +1,15 @@
 var express = require('express');
 var fs = require('fs');
-var buf = require('buffer');
+var htmlfile = "index.html"
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buf1 = new Buffer(fs.readFileSync('index.html'));
-  response.send(buf1.toString());
-  //response.send(buf.toString(fs.readFileSync('index.html')));
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
